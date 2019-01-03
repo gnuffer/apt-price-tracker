9613a2a3360e91d617c0c9893b6c1c1762fefc97
gerhardnuffer <gfnuffer@gmail.com>
2019-01-03 15:43:18 +0100

    Add Chrome Storage

--- content.js
+++ content.js
@@ -2,6 +2,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.greeting == "hello") {
         let header = document.getElementsByTagName('h1')[0];
         let title = header.textContent;
-        sendResponse({title: title});
+        chrome.storage.sync.set({title: title}, function() {
+            console.log('Title is set to ' + title);
+            sendResponse({title: title});
+        });
+
     }
 });
--- popup.js
+++ popup.js
@@ -5,10 +5,12 @@ trackButton.addEventListener('click', function() {
     // send message to content script
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-            console.log(response.title);
-            let headerElem = document.createElement('h1');
-            headerElem.textContent = response.title;
-            document.body.appendChild(headerElem);
+            chrome.storage.sync.get(['title'], function(result) {
+                console.log('Title currently is ' + result.title);
+                let headerElem = document.createElement('h1');
+                headerElem.textContent = result.title;
+                document.body.appendChild(headerElem);
+            });
         });
     });
 });
