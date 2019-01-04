715f1789ab3b020f9b47efb2fe8083fa3517f616
gerhardnuffer <gfnuffer@gmail.com>
2019-01-04 15:45:10 +0100

    Store and retrieve title and date

--- content.js
+++ content.js
@@ -2,8 +2,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.greeting == "hello") {
         let header = document.getElementsByTagName('h1')[0];
         let title = header.textContent;
-        chrome.storage.sync.set({title: title}, function() {
-            console.log('Title is set to ' + title);
+        let date = new Date();
+        let dateStr = date.toString();
+        let entryStr = title + '  ' + dateStr;
+        console.log(entryStr);
+        chrome.storage.sync.set({entry: entryStr}, function() {
+            console.log('The new entry is: ' + entryStr);
             sendResponse({title: title});
         });
 
--- popup.js
+++ popup.js
@@ -5,10 +5,10 @@ trackButton.addEventListener('click', function() {
     // send message to content script
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-            chrome.storage.sync.get(['title'], function(result) {
-                console.log('Title currently is ' + result.title);
+            chrome.storage.sync.get(['entry'], function(result) {
+                console.log('The new entry is ' + result.entry);
                 let headerElem = document.createElement('h1');
-                headerElem.textContent = result.title;
+                headerElem.textContent = result.entry;
                 document.body.appendChild(headerElem);
             });
         });
