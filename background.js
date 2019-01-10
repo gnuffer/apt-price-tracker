30f88d319c0b4e4477b1bad21c20c484f87db001
gerhardnuffer <gfnuffer@gmail.com>
2019-01-10 15:33:20 +0100

    Change pageUrl to www.immowelt.de

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
      chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
          chrome.declarativeContent.onPageChanged.addRules([{
              conditions: [new chrome.declarativeContent.PageStateMatcher({
-                 pageUrl: {hostEquals: 'developer.chrome.com'},
+                 pageUrl: {hostEquals: 'www.immowelt.de'},
              })
              ],
              actions: [new chrome.declarativeContent.ShowPageAction()]
--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.greeting == "hello") {
+        
+        // instead of getting header, get: zip, area, rooms, yearOfConstruction, price
         let header = document.getElementsByTagName('h1')[0];
         let title = header.textContent;
 
@@ -29,12 +31,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
         let dateStr = day + '.' + month + '.' + year + '   ' + hours + ':' + minutes + ' Uhr';
 
+        // instead of entryStr, let entryValue = [dateStr, zip, area, rooms, yearOfConstruction, price
         let entryStr = dateStr + ' -- ' + title;
         let entry = {};
+
+        // entry[dateStr] = entryValue;
         entry[dateStr] = entryStr;
         chrome.storage.local.set(entry, function() {
             console.log('The new entry is: ' + entryStr);
-            sendResponse({title: title});
+            sendResponse({title: title}); // replace with line 44?
         });
+        // sendResponse({currentDate: dateStr}); -- ???
     }
 });
