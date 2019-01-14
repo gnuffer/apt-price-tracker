1ce6056d77610c2c81fcabb8b08c30ad3bdb93bf
gerhardnuffer <gfnuffer@gmail.com>
2019-01-14 16:36:51 +0100

    Get date string with getDateStr()

--- content.js
+++ content.js
@@ -1,35 +1,40 @@
+function getDateStr() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (day < 10) {
+        day = '0' + day;
+        }
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    if (hours < 10) {
+            hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+            minutes = '0' + minutes;
+    }
+
+    let dateStr = day + '.' + month + '.' + year + '   ' + hours + ':' + minutes + ' Uhr';
+    return dateStr;
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.message == "Track button clicked") {
         
         let entryValue = [];
 
-        let date = new Date();
-	let day = date.getDate();
-        let month = date.getMonth() + 1;
-        let year = date.getFullYear();
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-
-        if (day < 10) {
-            day = '0' + day;
-            }
-
-        if (month < 10) {
-            month = '0' + month;
-        }
-
-        if (hours < 10) {
-                hours = '0' + hours;
-        }
-
-        if (minutes < 10) {
-                minutes = '0' + minutes;
-        }
-
-        let dateStr = day + '.' + month + '.' + year + '   ' + hours + ':' + minutes + ' Uhr';
+        let dateStr = getDateStr();
         entryValue.push(dateStr);
-
+        // let selectors = [{selector: '.location', subStr: 
         let locationElem = document.querySelector('.location');
         let locationStr = locationElem.querySelector('span').textContent;
         let zip = locationStr.trim().substring(0, 5);
