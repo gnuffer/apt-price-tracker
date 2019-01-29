aaf96fead37e73c8929ff57359b814296063a9ea
gerhardnuffer <gfnuffer@gmail.com>
2019-01-29 17:53:15 +0100

    Change popup depending on URL

--- popup.js
+++ popup.js
@@ -1,15 +1,11 @@
-// gets 'Track Price' button of popup
+// gets 'Track' button of popup
 let trackButton = document.getElementById('trackBtn');
+
+// gets 'Tracked' button of popup
+let trackedButton = document.getElementById('trackedBtn');
 
 // checks if a given array matches the current array at indices 1, 2, 3 (here: zip code, area, number of rooms)
 function matchesCurrent(array, current) {
-//    try {
-//        if (array.length !== current.length) {
-//            throw ('Arrays of unequal length cannot be compared!');
-//        }
-//    } catch (e) {
-//        console.log(e);
-//    }
     if (array.length !== current.length) {
         throw new Error('Arrays of unequal length cannot be compared!');
     } 
@@ -21,43 +17,75 @@ function matchesCurrent(array, current) {
     return true;
 }
 
-// listens for clicks on the 'Track price' button of popup
-trackButton.addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // sends message to content script
-        chrome.tabs.sendMessage(tabs[0].id, {message: "Track button clicked"}, function(response) {
+        // checks if current page contains the expose of a single apartment
+        if(tabs[0].url.indexOf('expose') > -1) {
+            console.log('You are looking at an expose');
 
-            // creates a date string from the response object sent back by content script
-            let dateStr = response.currentDate;
+            // hides trackedButton
+            trackedButton.hidden = true;
 
-            // retrieves everything stored in chrome storage (the entire storage object)
-            chrome.storage.local.get(null, function(result) {
+            // listens for clicks on the 'Track' button of popup
+            trackButton.addEventListener('click', function() {
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    // sends message to content script
+                    chrome.tabs.sendMessage(tabs[0].id, {message: "Track button clicked"}, function(response) {
 
-                // gets the storage entry created by the current click event
-                let current = result[dateStr];
+                        // creates a date string from the response object sent back by content script
+                        let dateStr = response.currentDate;
 
-                let listElem = document.createElement('ul');
+                        // retrieves everything stored in chrome storage (the entire storage object)
+                        chrome.storage.local.get(null, function(result) {
 
-                // iterates over the properties of the result object retrieved from storage and filters
-                // out those property values (arrays) that match the array of the currently clicked item.
-                for (let date in result) {
-                    if (result.hasOwnProperty(date) && matchesCurrent(result[date], current)) {
-                        let listItem = document.createElement('li');
-                        // creates date - price entry to be displayed in popup
-                        let listItemTextContent = result[date][0] + ' -- ' + result[date][4];
+                            // gets the storage entry created by the current click event
+                            let current = result[dateStr];
 
-                        // checks if price string starts with a digit.  If so, it appends the euro symbol
-                        if (!isNaN(parseInt(result[date][4][0], 10))) {
-                            listItemTextContent += ' \u20AC';
-                        } 
+                            let listElem = document.createElement('ul');
 
-                        let listItemValue = document.createTextNode(listItemTextContent);
-                        listItem.appendChild(listItemValue);
-                        listElem.appendChild(listItem);
-                    }
-                }
-                document.body.appendChild(listElem);
+                            // iterates over the properties of the result object retrieved from storage and filters
+                            // out those property values (arrays) that match the array of the currently clicked item.
+                            for (let date in result) {
+                                if (result.hasOwnProperty(date) && matchesCurrent(result[date], current)) {
+                                    let listItem = document.createElement('li');
+                                    // creates date - price entry to be displayed in popup
+                                    let listItemTextContent = result[date][0] + ' -- ' + result[date][4];
+
+                                    // checks if price string starts with a digit.  If so, it appends the euro symbol
+                                    if (!isNaN(parseInt(result[date][4][0], 10))) {
+                                        listItemTextContent += ' \u20AC';
+                                    } 
+
+                                    let listItemValue = document.createTextNode(listItemTextContent);
+                                    listItem.appendChild(listItemValue);
+                                    listElem.appendChild(listItem);
+                                }
+                            }
+                            document.body.appendChild(listElem);
+                        });
+                    });
+                });
             });
-        });
+          // checks if current page contains list with search results
+        } else if (tabs[0].url.indexOf('liste') > -1) {
+            console.log('You are looking at a list with search results');
+
+            // hides trackButton
+            trackButton.hidden = true;
+
+            trackedButton.addEventListener('click', function() {
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+                    // retrieves everything stored in chrome storage (the entire storage object)
+                    chrome.storage.local.get(null, function(result) {
+                        // TODO: build list of tracked items
+
+
+
+                    });
+                });
+            });
+        }
     });
 });
+
